b6e3f5a3cd7e7eff55b67f695bc739d5f89a8474
Dendrimer <franzjulius@gmail.com>
2018-07-18 21:15:52 -0400

    Add grpcweb for web client requests

--- cmd/metadata-server/run.go
+++ cmd/metadata-server/run.go
@@ -18,8 +18,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net"
+	"net/http"
 	"os"
 
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	"github.com/kurafs/kura/pkg/cli"
 	"github.com/kurafs/kura/pkg/log"
 	pb "github.com/kurafs/kura/pkg/pb/metadata"
@@ -38,10 +40,12 @@ Metadata server detailed overview.
 func metadataServerCmdRun(cmd *cli.Command, args []string) error {
 	var (
 		port        int
+		webPort     int
 		storageAddr string
 	)
 	cmd.FlagSet.StringVar(&storageAddr, "storage-addr", "localhost:10669", "Address of the storage server [host:port]")
 	cmd.FlagSet.IntVar(&port, "port", 10670, "Port which the server will run on")
+	cmd.FlagSet.IntVar(&webPort, "webPort", 10671, "Port which the browser server will run on")
 	if err := cmd.FlagSet.Parse(args); err != nil {
 		return cli.CmdParseError(err)
 	}
@@ -63,6 +67,23 @@ func metadataServerCmdRun(cmd *cli.Command, args []string) error {
 
 	gserver := grpc.NewServer()
 	pb.RegisterMetadataServiceServer(gserver, server)
+
+	// Serve to web as well
+	wrappedServer := grpcweb.WrapServer(gserver)
+	handler := func(resp http.ResponseWriter, req *http.Request) {
+		wrappedServer.ServeHTTP(resp, req)
+	}
+
+	httpServer := http.Server{
+		Addr:    fmt.Sprintf("127.0.0.1:%d", webPort),
+		Handler: http.HandlerFunc(handler),
+	}
+	go func() {
+		logger.Infof("Serving on web on port: %d", webPort)
+		if err := httpServer.ListenAndServe(); err != nil {
+			logger.Fatalf("failed starting http server: %v", err)
+		}
+	}()
 
 	logger.Infof("Serving on port: %d", port)
 	if err := gserver.Serve(lis); err != nil {
