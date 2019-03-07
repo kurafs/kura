d52c9c5f87b20b750e1a4ba9cd026a7ed3383e98
Dendrimer <franzjulius@gmail.com>
2019-03-07 14:30:00 -0500

    WIP

--- cmd/fuse-server/fs.go
+++ cmd/fuse-server/fs.go
@@ -2,7 +2,6 @@ package fuseserver
 
 import (
 	"context"
-	"io"
 	"os"
 	"sync"
 	"time"
@@ -10,12 +9,7 @@ import (
 	"github.com/kurafs/kura/pkg/fuse"
 	"github.com/kurafs/kura/pkg/fuse/fs"
 	"github.com/kurafs/kura/pkg/log"
-<<<<<<< HEAD
-	"github.com/kurafs/kura/pkg/streaming"
-
-=======
 	cpb "github.com/kurafs/kura/pkg/pb/crypt"
->>>>>>> master
 	mpb "github.com/kurafs/kura/pkg/pb/metadata"
 	"google.golang.org/grpc"
 )
@@ -158,50 +152,14 @@ func (f *File) ReadAll(ctx context.Context) ([]byte, error) {
 	f.parentDir.fuseServer.mu.Lock()
 	defer f.parentDir.fuseServer.mu.Unlock()
 
-	// TODO (Dendrimer): Use this as a chance to refresh metadata cache
-	metaReq := &mpb.GetMetadataRequest{Key: f.name}
-	metaResp, err := f.parentDir.fuseServer.metadataServerClient.GetMetadata(ctx, metaReq)
+	req := &mpb.GetFileRequest{Key: f.name}
+	res, err := f.parentDir.fuseServer.metadataServerClient.GetFile(ctx, req)
 	if err != nil {
 		return nil, err // TODO(irfansharif): Propagate appropriate FUSE error.
 	}
-	fileSize := metaResp.Metadata.Size
 
-	var contents string
-	if fileSize < streaming.Threshold {
-		req := &mpb.GetFileRequest{Key: f.name}
-		res, err := f.parentDir.fuseServer.metadataServerClient.GetFile(ctx, req)
-		if err != nil {
-			return nil, err // TODO(irfansharif): Propagate appropriate FUSE error.
-		}
-		contents = string(res.File)
-	} else {
-		req := &mpb.GetFileStreamRequest{Key: f.name}
-		stream, err := f.parentDir.fuseServer.metadataServerClient.GetFileStream(ctx, req)
-		if err != nil {
-			return nil, err
-		}
-		// TODO(Denrimer): Is it wise to be allocating huge amounts of memory
-		// up front? If we can decrypt from a stream then we don't have to do this.
-		buf := make([]byte, 0, fileSize)
-		for {
-			in, err := stream.Recv()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				return nil, err
-			}
-			buf = append(buf, in.FileChunk...)
-		}
-		contents = string(buf)
-	}
-
-<<<<<<< HEAD
-	decrypted, err := decrypt(pkey, contents)
-=======
 	creq := &cpb.DecryptionRequest{Ciphertext: res.File}
 	cres, err := f.parentDir.fuseServer.cryptServerClient.Decrypt(ctx, creq)
->>>>>>> master
 	if err != nil {
 		f.parentDir.fuseServer.logger.Error(err.Error())
 		return nil, err // TODO(irfansharif): Propagate appropriate FUSE error.
@@ -221,36 +179,6 @@ func (f *File) Write(ctx context.Context, req *fuse.WriteRequest, resp *fuse.Wri
 		return err // TODO(irfansharif): Propagate appropriate FUSE error.
 	}
 
-<<<<<<< HEAD
-	encBytes := []byte(encrypted)
-	if len(encBytes) < streaming.Threshold {
-		rq := &mpb.PutFileRequest{Key: f.name, File: encBytes}
-		_, err = f.parentDir.fuseServer.metadataServerClient.PutFile(ctx, rq)
-
-		if err != nil {
-			return err // TODO(irfansharif): Propagate appropriate FUSE error.
-		}
-	} else {
-		stream, err := f.parentDir.fuseServer.metadataServerClient.PutFileStream(ctx)
-		if err != nil {
-			return err // TODO: Propogate appropriate FUSE error
-		}
-		numChunks := len(encBytes) / streaming.ChunkSize
-		if len(encBytes)%streaming.ChunkSize != 0 {
-			numChunks++
-		}
-		for i := 0; i < numChunks; i++ {
-			b := i * streaming.ChunkSize
-			e := (i + 1) * streaming.ChunkSize
-			if e >= len(encBytes) {
-				e = len(encBytes) - 1
-			}
-
-			if err := stream.Send(&mpb.PutFileStreamRequest{Key: f.name, FileChunk: encBytes[b:e]}); err != nil {
-				return err
-			}
-		}
-=======
 	ts := time.Now().Unix()
 	metadata := &mpb.FileMetadata{
 		Size:         int64(len(req.Data)),
@@ -261,7 +189,6 @@ func (f *File) Write(ctx context.Context, req *fuse.WriteRequest, resp *fuse.Wri
 		Key:      f.name,
 		File:     []byte(cres.Ciphertext),
 		Metadata: metadata,
->>>>>>> master
 	}
 	_, err = f.parentDir.fuseServer.metadataServerClient.PutFile(ctx, rq)
 
